a4fc5e6ce328d580063059827dcf14088514a779
prodev0128 <prodev.development0128@gmail.com>
2024-12-10 08:00:23 -0800

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Allow CORS for all origins by default
   await app.listen(process.env.PORT ?? 3050);
 }
 
