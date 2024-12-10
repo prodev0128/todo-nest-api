1889212804cc096ac175f47a66c31dae08c6c59e
prodev0128 <prodev.development0128@gmail.com>
2024-12-10 05:36:51 -0800

    initial commit with mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/todo-app'),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
